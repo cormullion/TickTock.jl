51ea449d65afb7e6b46f4e24c7d3748f6086b975
cormullion <cormullion@mac.com>
2017-12-11 17:55:52 +0000

    first pass

--- src/TickTock.jl
+++ src/TickTock.jl
@@ -1,5 +1,29 @@
 module TickTock
 
-# package code goes here
+export tick, tock
+
+function tick()
+    t0 = time_ns()
+    task_local_storage(:TIMERS, (t0, get(task_local_storage(), :TIMERS, ())))
+    return t0
+end
+
+function toqk()
+    t1 = time_ns()
+    timers = get(task_local_storage(), :TIMERS, ())
+    if timers === ()
+        error("You tock()ed before you tick()ed...")
+    end
+    t0 = timers[1]::UInt64
+    task_local_storage(:TIMERS, timers[2])
+    (t1-t0)/1e9
+end
+
+function tock()
+    t = toqk()
+    dc = Dates.canonicalize(Dates.CompoundPeriod(now() - t))
+    println(dc)
+    return dc
+end
 
 end # module
